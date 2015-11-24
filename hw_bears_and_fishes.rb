3251462d9f60efdebc7ef75d8698d42b657e4091
Solomon Osiberu <lokobos67@gmail.com>
2015-11-24 08:32:18 +0000

    no improvement

---
+++ hw_bears_and_fishes.rb
@@ -0,0 +1,13 @@
+class Bears
+  def initalize(input_name, input_type)
+    @name = input_name
+    @type = input_type
+  end
+
+  def bear_roar
+    puts "Roar!"
+  end
+
+
+
+end
---
+++ hw_bears_and_fishes_runner.rb
@@ -0,0 +1,14 @@
+require_relative('hw_bears_and_fishes')
+bear_1 = TheBears.new('Peaches', 'Grizzly')
+bear_2 = TheBears.new('Starburst', 'Polar')
+bear_3 = TheBears.new('Da', 'Panda')
+
+fish_1 = Fish.new('Nemo')
+fish_2 = Fish.new('Dory')
+fish_3 = Fish.new('Marlin')
+
+bears = [bear_1, bear_2, bear_3]
+
+river = [fish_1, fish_2, fish_3]
+
+bear_roar
